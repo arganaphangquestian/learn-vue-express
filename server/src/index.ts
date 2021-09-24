0e20bc4bea4b5ebb1cb45100483c7bad329992ab
arganaphangquestian <arganaphangquestian@gmail.com>
2021-09-24 12:07:19 +0700

    Fix Config Nodemon

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,26 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 const PORT = 8000; // process.env.PORT
+const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
   res.json({ message: "App is Running" });
+});
+
+app.get("/post", async (req, res) => {
+  const posts = await prisma.post.findMany();
+  res.json({ message: "Get all post", data: { posts } });
+});
+
+app.get("/user", async (req, res) => {
+  const users = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+  res.json({ message: "Get all post", data: { users } });
 });
 
 app.listen(PORT, () => {
